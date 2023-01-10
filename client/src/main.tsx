6d4bac0fa7fa6b55109e00cdd8c20e704862d696
Yonatan Atias <56216286+yoni2528@users.noreply.github.com>
2023-01-10 21:45:15 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,9 +10,7 @@ import store from './store'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <BrowserRouter>
-      <React.StrictMode>
         <App />
-      </React.StrictMode>
     </BrowserRouter>
   </Provider>
 )
