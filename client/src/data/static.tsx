8be2e4e42b46d3c8dcb65e41cbf0076167e78358
yoni2528 <atiasyehonatan@gmail.com>
2023-01-15 14:47:57 +0200

    App Deployment

--- client/src/data/static.tsx
+++ client/src/data/static.tsx
@@ -1,4 +1,5 @@
-export const BASE_URL = 'https://cryptofolio-b4di.onrender.com/'
+export const BASE_URL = 'https://cryptofolio.up.railway.app/'
+export const BASE_URL2 = 'https://cryptofolio-b4di.onrender.com/'
 
 export const MONTHS = [
   'January',
